8defeca5c4be01e29c9160717a5ad96a86a9e511
cbstodd <cbstodd@gmail.com>
2017-04-05 21:17:46 -0400

    added webpack

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+const React          = require('react'),
+      ReactDOM       = require('react-dom');    
+
+
+ReactDOM.render(
+  <h1>Boilerplate app!</h1>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    devtool: 'source-map',
+    entry:   './app/app.jsx',
+    // Places files from loaders (what gets rendered).
+    output:  {
+        path:     __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+        //   ClassName:  'file/path',
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module:  {
+        loaders: [
+            // Says to load any .js file from /src
+            {
+                test:    /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+//                include: path.resolve(__dirname, 'public'),
+                loader:  'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+    watch: true
+};
